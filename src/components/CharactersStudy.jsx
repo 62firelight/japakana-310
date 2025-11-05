aaac954dcedee72e1c1bf60fa38895285e8c1479
62firelight <lktfirelight@gmail.com>
2025-11-05 14:25:31 +1300

    Add summary at end of study session

--- src/components/CharactersStudy.jsx
+++ src/components/CharactersStudy.jsx
@@ -4,12 +4,17 @@ import ANSWERS from "../util/Answers";
 function CharactersStudy(props) {
     const charactersToStudy = props.selectedChars;
 
+    // Question tracking
     const [currentCharQuestion, setCurrentCharQuestion] = useState(
         props.studyMode ? charactersToStudy[0] : ""
     );
     const [currentAnswer, setCurrentAnswer] = useState("");
     const [progressIndex, setProgressIndex] = useState(0);
     const [answerStatus, setAnswerStatus] = useState("");
+
+    // Correct/incorrect answer tracking
+    const [answerSummary, setAnswerSummary] = useState({});
+    const [answerSummaryComponent, setAnswerSummaryComponent] = useState("");
 
     function resetProgress(fullReset = false) {
         setProgressIndex(0);
@@ -32,8 +37,16 @@ function CharactersStudy(props) {
             ANSWERS.getKey(currentCharQuestion) == currentAnswer
         ) {
             setAnswerStatus("Correct! Press ENTER to continue.");
+            setAnswerSummary({
+                ...answerSummary,
+                [currentCharQuestion]: true,
+            });
         } else {
             setAnswerStatus("Incorrect! Press ENTER to continue.");
+            setAnswerSummary({
+                ...answerSummary,
+                [currentCharQuestion]: false,
+            });
         }
     }
 
@@ -55,9 +68,29 @@ function CharactersStudy(props) {
             setAnswerStatus("");
             setCurrentAnswer("");
             props.setSelectedChars([]);
-            resetProgress(true);
-            props.setStudyMode(false);
+
+            setAnswerSummaryComponent(
+                Object.entries(answerSummary).map(([key, value]) => {
+                    return (
+                        <li>
+                            {key}{" "}
+                            {value ? (
+                                <span className="correct">Correct</span>
+                            ) : (
+                                <span className="incorrect">Incorrect</span>
+                            )}
+                        </li>
+                    );
+                })
+            );
         }
+    }
+
+    function finishStudySession() {
+        resetProgress(true);
+        setAnswerSummary({});
+        setAnswerSummaryComponent("");
+        props.setStudyMode(false);
     }
 
     if (props.isVisible) {
@@ -65,34 +98,48 @@ function CharactersStudy(props) {
             <div>
                 <h1>Study</h1>
 
-                <form className="character-study" onSubmit={handleSubmit}>
-                    <div className="progress-indicator">
-                        {progressIndex + 1} / {charactersToStudy.length}
+                {answerSummaryComponent.length <= 0 ? (
+                    <form className="character-study" onSubmit={handleSubmit}>
+                        <div className="progress-indicator">
+                            {progressIndex + 1} / {charactersToStudy.length}
+                        </div>
+
+                        <div className="current-char">
+                            {currentCharQuestion}
+                        </div>
+
+                        <div
+                            className={
+                                answerStatus[0] == "C" ? "correct" : "incorrect"
+                            }
+                        >
+                            {answerStatus}
+                        </div>
+
+                        <input
+                            type="text"
+                            id="answer-input"
+                            name="text"
+                            autoComplete="off"
+                            value={currentAnswer}
+                            onChange={handleChange}
+                            readOnly={answerStatus.length > 0}
+                            placeholder="Enter the English translation"
+                        />
+
+                        <button type="submit">Submit</button>
+                    </form>
+                ) : (
+                    <div className="summary">
+                        <button onClick={finishStudySession}>Finish</button>
+
+                        <h2>Answer Summary</h2>
+
+                        <ul className="answer-summary">
+                            {answerSummaryComponent}
+                        </ul>
                     </div>
-
-                    <div className="current-char">{currentCharQuestion}</div>
-
-                    <div
-                        className={
-                            answerStatus[0] == "C" ? "correct" : "incorrect"
-                        }
-                    >
-                        {answerStatus}
-                    </div>
-
-                    <input
-                        type="text"
-                        id="answer-input"
-                        name="text"
-                        autoComplete="off"
-                        value={currentAnswer}
-                        onChange={handleChange}
-                        readOnly={answerStatus.length > 0}
-                        placeholder="Enter the English translation"
-                    />
-
-                    <button type="submit">Submit</button>
-                </form>
+                )}
 
                 {/* <h2>Selected Characters</h2>
 
